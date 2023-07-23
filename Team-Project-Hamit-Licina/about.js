92139055b53e0ebf50c3618db6a7f3c66a4a20b6
hamitLicina <hamitlicina@gmail.com>
2023-07-23 13:48:30 +0300

    about page added, open to your suggestions

---
+++ Team-Project-Hamit-Licina/about.js
@@ -0,0 +1,14 @@
+const teamMembers = [{
+    name: "Hamit",
+    imageUrl: "https://scontent.flhr1-1.fna.fbcdn.net/v/t39.30808-6/359858413_614185684105396_311404432361238530_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=IvHq0XZSadUAX-djlD1&_nc_ht=scontent.flhr1-1.fna&oh=00_AfDJEQJq-RLRtq_0cscI_ktQRZFdOcxAmtgFAAnheFilUA&oe=64BDBCD4",
+
+}]
+
+
+apiKey = 'a3ed24058c6960422612f376ca988d6bdc6d4a67'
+function trial(){
+    const data = "";
+    console.log(apiKey);
+
+}
+trial();
